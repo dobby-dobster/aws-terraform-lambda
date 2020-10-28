b6ce4a741be24f4d0aa07fa88a02de000ddcb51a
dobby-dobster <tom@tbaker.net>
2020-10-28 12:33:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_version = ">=0.12.0"
-  required_providers {
-    aws = {
-      version = ">=3.0.0"
-    }
-  }
-  backend "s3" {
-    region  = "us-east-1"
-    profile = "default"
-    key     = "terraformstatefile"
-    bucket  = "aws-terraform-ansible-jenkins-env"
-  }
-}
-
-
-provider "aws" {
-  profile = default
-  region  = us-east-1
-}
-
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -41,13 +20,13 @@ EOF
 
 resource "aws_lambda_function" "hello_world_lambda" {
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_handler"
+  function_name = "hello_world_python"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "exports.test"
+  handler       = "hello-world.lambda_handler"
 
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "Python 3.8"
+  runtime = "python3.8"
 
   environment {
     variables = {
