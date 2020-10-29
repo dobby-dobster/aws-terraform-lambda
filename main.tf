720e809686e2973754f95bcafdd01fd0dd297f60
dobby-dobster <tom@tbaker.net>
2020-10-29 18:33:57 +0000

    Adding API Gateway code

--- main.tf
+++ main.tf
@@ -34,3 +34,29 @@ resource "aws_lambda_function" "hello_world_lambda" {
     }
   }
 }
+
+# HTTP API
+resource "aws_apigatewayv2_api" "api" {
+	name          = "hello_world_python"
+	protocol_type = "HTTP"
+	target        = aws_lambda_function.hello_world_lambda.arn
+}
+
+# Permission
+resource "aws_lambda_permission" "apigw" {
+	action        = "lambda:InvokeFunction"
+	function_name = aws_lambda_function.hello_world_lambda.arn
+	principal     = "apigateway.amazonaws.com"
+
+	source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /"
+}
+
+resource "aws_apigatewayv2_route" "route_hello" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /hello"
+}
